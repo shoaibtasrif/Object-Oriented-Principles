5227e10b32d8d146d06f3fa76f085ffbadd28135
Md. Shoaib Tasrif <shoaib01@student.sust.edu>
2022-06-09 14:49:33 +0600

    Initial commit

---
+++ Socket Programming/Client.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.maven_java;
+
+/**
+ *
+ * @author shoai
+ */
+// A Java program for a Client
+import java.net.*;
+import java.io.*;
+
+public class Client
+{
+	// initialize socket and input output streams
+	private Socket socket		 = null;
+	private DataInputStream input = null;
+	private DataOutputStream out	 = null;
+
+	// constructor to put ip address and port
+	public Client(String address, int port)
+	{
+		// establish a connection
+		try
+		{
+			socket = new Socket(address, port);
+			System.out.println("Connected");
+
+			// takes input from terminal
+			input = new DataInputStream(System.in);
+
+			// sends output to the socket
+			out = new DataOutputStream(socket.getOutputStream());
+		}
+		catch(UnknownHostException u)
+		{
+			System.out.println(u);
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+
+		// string to read message from input
+		String line = "";
+
+		// keep reading until "Over" is input
+		while (!line.equals("Over"))
+		{
+			try
+			{
+				line = input.readLine();
+				out.writeUTF(line);
+			}
+			catch(IOException i)
+			{
+				System.out.println(i);
+			}
+		}
+
+		// close the connection
+		try
+		{
+			input.close();
+			out.close();
+			socket.close();
+		}
+		catch(IOException i)
+		{
+			System.out.println(i);
+		}
+	}
+
+	public static void main(String args[])
+	{
+		Client client = new Client("127.0.0.1", 5000);
+	}
+}
+
---
+++ Socket Programming/Maven_java.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.maven_java;
+
+/**
+ * State pattern - refactoringguru.com
+ * @author shoai
+ */
+
+
+/**
+ * World first console e-commerce application.
+ */
+
+public class Maven_java {
+    public static void main(String[] args) throws InterruptedException {
+        
+    }
+}
